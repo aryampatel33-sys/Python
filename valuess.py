7e8cc06a0d41fdbd83e82b2d3e8fa9b1893dfa5c
aryampatel33-sys <aryampatel33@gmail.com>
2026-08-04 18:40:54 +1200

    Add files via upload

---
+++ valuess.py
@@ -0,0 +1,18 @@
+a = 13
+b = 23
+c = 0
+if a and b and c:
+    print("All the numbers have their boolean value as true.")
+else:
+    print("Atleast one number has their boolean value as false")
+a = 13
+b = -3
+c = 0
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+if b > 0 or c > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
